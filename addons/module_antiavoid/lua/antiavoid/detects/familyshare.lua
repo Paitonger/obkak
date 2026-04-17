b6bebdea47bbe4843306d032211ae2de55423f01
Rayz1337 <96474993+Rayz1337@users.noreply.github.com>
2026-04-17 16:57:51 +0500

    Delete addons/module_antiavoid/lua directory
    хуйте а не авойдич

--- addons/module_antiavoid/lua/antiavoid/detects/familyshare.lua
+++
@@ -1,9 +0,0 @@
--- t.me/urbanichka
-antiAvoid.addDetect('fs', function (ply)
-    local sid = ply:SteamID64()
-    local ownerId = ply:OwnerSteamID64()
-
-    if ownerId ~= sid and ownerId ~= '0' then
-        antiAvoid.saveAccount(util.SteamIDFrom64(sid), util.SteamIDFrom64(ownerId), 'fs')
-    end
-end)
