b5c7a13d87630b875be871bac8ec842e10f56858
Rayz1337 <96474993+Rayz1337@users.noreply.github.com>
2026-04-17 19:49:32 +0500

    Disable outfit hooks and purchase checks
    закоментил потому что убрали аутфиттер

--- addons/_smallscripts/lua/autorun/server/sv_outfit.lua
+++ addons/_smallscripts/lua/autorun/server/sv_outfit.lua
@@ -1,26 +1,26 @@
 -- 17.04
-hook.Add( "CanOutfit", "srp_donate.wsmodels", function( ply, mdl, wsid )
+--hook.Add( "CanOutfit", "srp_donate.wsmodels", function( ply, mdl, wsid )
 
-    if not ply:HasPurchase("skinworkshop") then
-        DarkRP.notify(ply, NOTIFY_ERROR, 3, "Ты не купил эту плюшку!")
-        return false
-    end
+  --  if not ply:HasPurchase("skinworkshop") then
+  --      DarkRP.notify(ply, NOTIFY_ERROR, 3, "Ты не купил эту плюшку!")
+  --      return false
+  --  end
     
-    return true
+   -- return true
 
-end)
+--end)
 
-hook.Add("PlayerSay", "srp_donate.wsmodels", function(ply, text)
+--hook.Add("PlayerSay", "srp_donate.wsmodels", function(ply, text)
 
-    text = string.Explode(" ", text)
-    if text[1] == "!mymodel" then
-        if not ply:HasPurchase("skinworkshop") then
-                DarkRP.notify(ply, NOTIFY_ERROR, 3, "Ты не купил эту плюшку!")
-                return false
-        end
+  --  text = string.Explode(" ", text)
+  --  if text[1] == "!mymodel" then
+    --    if not ply:HasPurchase("skinworkshop") then
+      --          DarkRP.notify(ply, NOTIFY_ERROR, 3, "Ты не купил эту плюшку!")
+     --           return false
+      --  end
 
-        ply:SendLua("outfitter.GUIOpen()")
-        return ""
-    end
+      --  ply:SendLua("outfitter.GUIOpen()")
+      --  return ""
+   -- end
 
-end)
+--end)
